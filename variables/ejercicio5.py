66e46f03fbac60c59042257bf539003cb099758c
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-09-29 17:31:08 -0400

    Ejercicio5 de ejercicios de prograación

--- variables/ejercicio5.py
+++ variables/ejercicio5.py
@@ -3,6 +3,6 @@ nombre = input("¿cual es tu nombre? :")
 horas_trabajadas = input("¿cuantas horas trabajaste? :")
 paga_por_hora = input("¿cuanto cobras por hora? :")
 mensaje = "tu monto a cobrar es"
-monto_a_cobrar = int(pagaporhora)*int(horas_trabajadas)
+monto_a_cobrar = int(paga_por_hora)*int(horas_trabajadas)
 d = "dólares"
 print(saludo, nombre, mensaje, monto_a_cobrar, d)
