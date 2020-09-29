6928287f2cfa8c693e28a0d2fa8e54831812fab2
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-09-29 17:14:03 -0400

    Ejercicio6 de ejercicios de prograación

--- variables/ejercicio 6.py
+++ variables/ejercicio 6.py
@@ -1,7 +1,7 @@
 saludo = "buenos dias!"
 nombre = input("¿comó te llamas? :")
-peso = input("porfavor ingrese su peso en \"kg\"")
-estatura = input("porfavor ingrese su estatura en \"m\"")
+peso = input("porfavor ingrese su peso en \"kg\":")
+estatura = input("porfavor ingrese su estatura en \"m\":")
 mensaje = "tu indice de masa corporal es"
-imc = int(peso)/float(estatura)
+imc = float(peso)/float(estatura)
 print(saludo, nombre, mensaje, imc)
