8b3898e522d91204935a4c43cd3496c10b1d0134
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-09-29 17:29:38 -0400

    Ejercicio7 de ejercicios de prograación

---
+++ variables/ejercicio 7.py
@@ -0,0 +1,6 @@
+ingreso = input("Ingrese un numero entero: ")
+operacion_1 = int(ingreso)+1
+operacion_2 = int(operacion_1)*int(ingreso)
+operacion_3 = int(operacion_2)/2
+mensaje= "La suma de los numeros enteros es"
+print(mensaje, operacion_3)
