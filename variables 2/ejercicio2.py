38286ae1e80fccf44ef8ce0f0213061f0ad6a6dc
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-10-01 16:22:57 -0400

    ejercicio2 de variables, operadores y entrada/salida

--- variables 2/ejercicio2.py
+++ variables 2/ejercicio2.py
@@ -1,5 +1,5 @@
 dividendo = int(input("ingrese el dividendo :"))
 divisor = int(input("ingrese el divisor :"))
-operacion = dividendo%divisor
+operacion = dividendo % divisor
 mensaje = f"El resto obtenido al dividir {dividendo} entre {divisor} es {operacion}"
 print(mensaje)
