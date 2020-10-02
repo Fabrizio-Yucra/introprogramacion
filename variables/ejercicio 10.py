cfef40483eb60c0715d9f54f5ccba0044c076fe4
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-10-02 13:10:35 -0400

    Ejercicio 10 de ejercicios de programación

---
+++ variables/ejercicio 10.py
@@ -0,0 +1,7 @@
+numero = int(input("ingrese un numero: "))
+nombre = str(input("ingrese su nombre: "))
+operacion = numero % 7
+if operacion == 0 and nombre == str("Juan"):
+    print("Exito!!!")
+if operacion == 0 and nombre == str("Pablo"):
+    print("Exito!!!")
