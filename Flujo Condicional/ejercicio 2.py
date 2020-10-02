4729ee9b365c07df5c1d1a5a6e3926441e048054
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-10-02 16:41:37 -0400

    Ejercicio 2 de Instrucciones de flujo condicional.

---
+++ Flujo Condicional/ejercicio 2.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Ingrese un numero: "))
+numero2 = int(input("Ingrese un segundo numero: "))
+if numero1 > numero2:
+    print(numero1)
+else:
+    numero2 > numero2
+    print(numero2)
