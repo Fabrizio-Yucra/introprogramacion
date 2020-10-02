dd84501a31a2537301c015e3552f38a58ee62d5e
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-10-02 11:57:02 -0400

    ejercicio4 de Variables, operadores y entrada/salida

---
+++ variables 2/ejercicio4.py
@@ -0,0 +1,10 @@
+numero_1 = float(input("ingrese un numero: "))
+numero_2 = float(input("ingrese un un segundo numero: "))
+operacion_1 = numero_1 == numero_2
+print(f"Los numeros son iguales: {operacion_1}")
+operacion_2 = numero_1 != numero_2
+print(f"Los numeros no son iguales: {operacion_2}")
+operacion_3 = numero_1 > numero_2
+print(f"El primer numero es mayor al segundo: {operacion_3}")
+operacion_4 = numero_1 < numero_2
+print(f"El segundo numero es mayor al primero: {operacion_4}")
