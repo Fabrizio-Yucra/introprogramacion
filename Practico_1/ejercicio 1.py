927a12f7160d5bebeb38b01305af4cba6d8d5536
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-10-13 11:26:17 -0400

    Practico; ejercicio 1

---
+++ Practico_1/ejercicio 1.py
@@ -0,0 +1,7 @@
+numero = float(input("Ingrese un numero: "))
+if numero == 0:
+    print("Su numero es 0.")
+elif numero % 2 == 0:
+    print("Su numero es par.")
+else:
+    print("Su numero es impar.")
