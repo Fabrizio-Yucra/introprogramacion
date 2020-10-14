2ebc534d46b4b5cbb3a1beeaa3f037e5b6abb036
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-10-14 12:30:29 -0400

    Ejercicio del Practico mejorado para el valor de cero.

--- Practico_1/ejercicio 15.2.py
+++ Practico_1/ejercicio 15.2.py
@@ -2,20 +2,23 @@ numero = int(input("Ingrese un numero: "))
 suma = 2
 resultado = 0
 f = f"*"
-print(1, end=" ")
-while suma <= numero:
-    resultado = suma
-    var_1 = suma - suma + 1
-    print(var_1 * f)
-    print(suma, end=" ")
-    while resultado != 1:
-        if resultado % 2 == 0:
-            resultado = int(resultado / 2)
-            var = resultado - resultado + 1
-            print(var * f, end=" ")
-        else:
-            resultado = int((resultado * 3) + 1)
-            var = resultado - resultado + 1
-            print(var * f, end=" ")
-    suma = suma + 1
-print("*")
+if numero == 0:
+    print(0)
+else:
+    print(1, end=" ")
+    while suma <= numero:
+        resultado = suma
+        var_1 = suma - suma + 1
+        print(var_1 * f)
+        print(suma, end=" ")
+        while resultado != 1:
+            if resultado % 2 == 0:
+                resultado = int(resultado / 2)
+                var = resultado - resultado + 1
+                print(var * f, end=" ")
+            else:
+                resultado = int((resultado * 3) + 1)
+                var = resultado - resultado + 1
+                print(var * f, end=" ")
+        suma = suma + 1
+    print("*")
