35bafafc23881cfcc301933a3bd1d35dd7acb2c2
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-10-22 16:16:06 -0400

    ejercicio1 de fnciones

---
+++ Funciones tabla de multiplicar/ejercicio 1.py
@@ -0,0 +1,17 @@
+def tabla(numero):
+    print()
+    print(f"Tabla del {numero}")
+    print()
+    for i in range(1, 11):
+        print(f"{numero} * {i} : {numero * i}")
+
+tabla(1)
+tabla(2)
+tabla(3)
+tabla(4)
+tabla(5)
+tabla(6)
+tabla(7)
+tabla(8)
+tabla(9)
+tabla(10)
