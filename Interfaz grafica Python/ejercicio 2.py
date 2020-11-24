cefdaaa09147984d2078b6144204749b810005ef
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-11-24 18:44:46 -0400

    ejercicio 2

--- Interfaz grafica Python/ejercicio 2.py
+++ Interfaz grafica Python/ejercicio 2.py
@@ -1,19 +1,25 @@
 v1 = []
 contador = 1
-suma = 0
+
 while contador <= 5:
     num = int(input("Ingrese un numero para el vector 1: "))
     v1.append(num)
     contador += 1
-    suma = suma + num
-suma2 = 0
+print("")
 v2 = []
 contador2 = 1
 while contador2 <= 5:
     num2 = int(input("Ingrese un numero para el vector 2: "))
     v2.append(num2)
     contador2 += 1
-    suma2 = suma2 + num2
+print("")
+vr = []
+contador3 = 0
+while contador3 <= 4:
+    resultado = int(v1[contador3]) + int(v2[contador3])
+    vr.append(resultado)
+    contador3 += 1
 
-operacion = suma + suma2
-print(f"La suma de los vectores es {operacion}")
+print(f"El vector 1 es: {v1}")
+print(f"El vector 2 es: {v2}")
+print(f"La suma de los vectores es {vr}")
