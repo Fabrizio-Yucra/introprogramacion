20b4a0530e98a9c1ec64794aa3cdec13d673114d
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-11-27 16:48:59 -0400

    2.0

--- Interfaz grafica Python/ejercicio 3.py
+++ Interfaz grafica Python/ejercicio 3.py
@@ -1,17 +1,18 @@
 import random
 
 numero = int(input("Ingrese el tamaño de su Lista: "))
+numv = int(input("Ingrese el numero del ultimo dijito: "))
 contador = 1
 v2 = []
 v3 = []
 while contador <= numero:
     v1 = random.randint(1, 300)
     a = str(v1)
-    if a[-1] == str(5):
+    if a[-1] == str(numv):
         v3.append(v1)
     v2.append(v1)
     contador += 1
 print("")
 print(f"Su lista aleatoria de {numero} es: \n{v2}")
 print("")
-print(f"Numeros aleatorios de la lista terminados en 5 son: \n{v3}")
+print(f"Numeros aleatorios de la lista terminados en {numv} son: \n{v3}")
