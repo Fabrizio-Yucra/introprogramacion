8ce9720e1862c7fc6b8fd91b3b659a326d851418
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-12-01 16:57:12 -0400

    ejercicio 2

---
+++ Listas (Array)/ejercicio 2.py
@@ -0,0 +1,20 @@
+num_I = int(input("Ingrese un número inicial: "))
+num_F = int(input("Ingrese un número final: "))
+l1 = []
+lp = []
+suma1 = 0
+li = []
+suma2 = 0
+for a in range(num_I, num_F - 1, -1):
+    l1.append(a)
+    if a % 2:
+        lp.append(a)
+        suma1 = suma1 + a
+    else:
+        li.append(a)
+        suma2 = suma2 + a
+print(l1)
+print(f"Lista Par: {li}")
+print(f"Suma: {suma2}")
+print(f"Lista Impar: {lp}")
+print(f"Suma: {suma1}")
