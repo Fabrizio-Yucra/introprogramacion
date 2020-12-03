9136bda78e2370277ff0baa3ce1f2ea3b9250e71
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-12-03 17:26:18 -0400

    ejercicio 3

--- Listas (Array)/ejercicio 3.py
+++ Listas (Array)/ejercicio 3.py
@@ -11,18 +11,18 @@ else:
     lm = base
 no_entran = []
 si_entran = []
-while numero != 0:
+
+while numero > 0:
     radio = int(input("Ingrese un radio: "))
     numero = radio
     if numero == 0:
         numero = 0
     diametro = 2 * radio
     if diametro <= lm:
-        lm = lm - diametro
         lM = lM - diametro
         si_entran.append(radio)
     else:
-        lm = lm
         no_entran.append(radio)
+
 print(f"Los circulos que si pueden entrar a la figura son {si_entran}")
 print(f"Los circulos que no pueden entrar a la figura son {no_entran}")
