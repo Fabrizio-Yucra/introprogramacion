a63ecd13650d9647e6db0ff15b07aecb3ef7de58
Fabrizio_yucra <Fabrizio.yucra@ucb.edu.bo>
2020-12-03 18:05:16 -0400

    ejercicio 3

--- Listas (Array)/ejercicio 3.py
+++ Listas (Array)/ejercicio 3.py
@@ -1,5 +1,5 @@
-base = int(input("Ingrese la base de su figura: "))
-altura = int(input("Ingrese la altura de su figura: "))
+base = float(input("Ingrese la base de su figura: "))
+altura = float(input("Ingrese la altura de su figura: "))
 numero = 1
 lM = 0
 lm = 0
@@ -13,16 +13,18 @@ no_entran = []
 si_entran = []
 
 while numero > 0:
-    radio = int(input("Ingrese un radio: "))
+    radio = float(input("Ingrese un radio: "))
     numero = radio
     if numero == 0:
         numero = 0
     diametro = 2 * radio
-    if diametro <= lm:
-        lM = lM - diametro
-        si_entran.append(radio)
+    if lM > 0:
+        if diametro <= lm:
+            lM = lM - diametro
+            si_entran.append(radio)
+        else:
+            no_entran.append(radio)
     else:
         no_entran.append(radio)
-
 print(f"Los circulos que si pueden entrar a la figura son {si_entran}")
 print(f"Los circulos que no pueden entrar a la figura son {no_entran}")
